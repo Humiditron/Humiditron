bbedc2cf773125257b743a0ccde8a2c25548f36b
Humiditron <223341941+Humiditron@users.noreply.github.com>
2026-09-07 20:30:55 -0400

    Create template.h

---
+++ templates/template.h
@@ -0,0 +1,15 @@
+/**
+ * @file           main.cpp
+ * @brief          [Brief module summary]
+ * @project        HUMID1_OS
+ * @author         Humiditron
+ * @Orfanization   Humidyne Labs
+ * @date           2026-09-07
+ * @license        MIT
+ *
+ * @target         ESP32 / ESP32-S3
+ * @peripherals I2C, SPI, BLE, MQTT
+ *
+ * @details        [Detailed description of functionality, task handling,
+ *                  or hardware memory management notes.]
+ */
